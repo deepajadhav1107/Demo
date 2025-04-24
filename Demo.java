c3e8c1d782913570e58aa0ea9e5cb4487d577fcf
deepajadhav1107 <deepajadhav2001@gmailcom>
2025-04-24 11:59:07 +0530

    savethis

--- Demo.java
+++ Demo.java
@@ -5,7 +5,11 @@ class Demo
         int b=25;
         int sum=a+b;
         int sub=a-b;
+        int mul=a*b;
+        int div=a/b;
         System.out.println("sum is: "+sum);
         System.out.println("sub is: "+sub);
+        System.out.println("mulis: "+mul);
+        System.out.println("div is: "+div);
     }
 }
